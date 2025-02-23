1c151e6d70e53cafc21fde231e39a9b30343b47e
bourbonut <ben.bourbon06@gmail.com>
2025-02-23 10:51:49 +0100

    Add `test_symbol.py`

---
+++ tests/utils/test_symbol.py
@@ -0,0 +1,18 @@
+from plotynium.utils.symbol import Symbol
+from detroit.scale.ordinal import ScaleOrdinal
+
+def test_symbol_init():
+    symbol = Symbol([{"foo": x} for x in "cba"], "foo")
+    assert symbol._labels == ["a", "b", "c"]
+    assert isinstance(symbol._symbol_type, ScaleOrdinal)
+
+def test_symbol_try_init():
+    assert Symbol.try_init([], None) is None
+    value = lambda d: d
+    assert Symbol.try_init([], value) == value
+    symbol = Symbol.try_init([{"foo": x} for x in "cba"], "foo")
+    assert isinstance(symbol, Symbol)
+
+def test_symbol_call():
+    symbol = Symbol([{"foo": x} for x in "cba"], "foo")
+    assert isinstance(symbol({"foo": "a"}), str)
