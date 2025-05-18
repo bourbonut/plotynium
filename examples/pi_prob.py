029de2fcd6f03783686ab008d2809a319b36d6ef
bourbonut <ben.bourbon06@gmail.com>
2025-05-18 18:16:43 +0200

    Update `pi_prob.py` in `examples`

--- examples/pi_prob.py
+++ examples/pi_prob.py
@@ -10,14 +10,15 @@ scheme = ply.Scheme.OBSERVABLE_10
 plot = ply.plot(
     width=500,
     height=500,
-    margin_bottom=30,
-    margin_top=30,
+    margin_top=10,
     margin_left=30,
-    margin_right=30,
+    margin_bottom=20,
+    margin_right=10,
     marks=[ply.dot(data, stroke=lambda d: scheme(bool(hypot(*d) < 1)))],
     # color={"legend": True},
     x={"nice": True},
     y={"nice": True},
+    color={"legend": True, "labels": {0: "Inside", 1: "Outside"}},
 )
 
 with open("pi.svg", "w") as file:
