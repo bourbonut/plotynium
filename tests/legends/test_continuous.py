2c3407d9855a91c8e9e15bbd81121f1ac99371e5
bourbonut <ben.bourbon06@gmail.com>
2025-05-25 18:06:38 +0200

    Add legend tests

---
+++ tests/legends/test_continuous.py
@@ -0,0 +1,13 @@
+from plotynium.legends import Legend
+from tests.default_context import default_context
+import detroit as d3
+
+def test_continuous_legend():
+    legend = Legend()
+
+    svg = d3.create("svg")
+    ctx = default_context(d3.scale_linear(), d3.scale_linear())
+    legend.apply(svg, ctx)
+    assert len(svg.select_all("rect").nodes()) == 105
+    assert len(svg.select_all("path").nodes()) == 6
+    assert len(svg.select_all("text").nodes()) == 6
--- tests/legends/test_discrete.py
+++ tests/legends/test_discrete.py
@@ -2,14 +2,10 @@ from plotynium.legends import Legend
 from tests.default_context import default_context
 import detroit as d3
 
-def test_symbol_legend():
+def test_discrete_legend():
     colors = ("blue", "red", "green")
-    symbols = d3.scale_ordinal(
-        [f"label_{i}" for i in range(len(colors))], d3.SYMBOLS_STROKE
-    )
     legend = Legend(
-        [(f"label_{i}", color) for i, color in enumerate(colors)],
-        [(f"label_{i}", d3.symbol(symbols(f"label_{i}"))()) for i in range(len(colors))]
+        [(f"label_{i}", color) for i, color in enumerate(colors)]
     )
 
     svg = d3.create("svg")
@@ -17,5 +13,5 @@ def test_symbol_legend():
     legend.apply(svg, ctx)
     g = svg.select("g.legend").select_all("g")
     assert len(g.nodes()) == 3
-    assert len(g.select_all("path").nodes()) == 3
+    assert len(g.select_all("rect").nodes()) == 3
     assert len(g.select_all("text").nodes()) == 3
