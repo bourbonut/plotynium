cef42ae60c3e316ba1c7e12a7918e3cdfa5aef29
bourbonut <ben.bourbon06@gmail.com>
2025-05-27 20:58:31 +0200

    Add docstring to `check_types` function

--- plotynium/marks/__init__.py
+++ plotynium/marks/__init__.py
@@ -27,6 +27,21 @@ __all__ = [
 
 
 def check_types(*types: list[type[Mark]]) -> Callable[[Mark], bool]:
+    """
+    Builds a function for checking different mark types
+
+
+    Parameters
+    ----------
+    types : list[type[Mark]]
+        Mark types to be checked
+
+    Returns
+    -------
+    Callable[[Mark], bool]
+        Function takes a `Mark` object as input and returns if the object is
+        one of the types given by the `check_types` function itself.
+    """
     def check_mark(mark: Mark) -> bool:
         return isinstance(mark, tuple(types))
 
